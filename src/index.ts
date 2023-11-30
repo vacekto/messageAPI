2a9e221cb117bb92c90fc5a25b1b18b01e07bdc9
Tom Vacek <tomas.vacek@avvoka.com>
2023-11-30 14:55:16 +0100

    added express server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import connectToRedis from "./redis/connect"
 import connectToMongo from './mongo/connect'
+import express from "express"
 
-connectToMongo()
-connectToRedis()
+const app = express()
+
+app.get('/test', (req, res) => {
+    console.log('TESTING')
+    res.status(200)
+})
+
+app.listen(3000, ()=>{
+    console.log(`server running on port ${process.env.PORT_SERVER} in mode: `, process.env.NODE_ENV)
+    connectToMongo()
+    connectToRedis()
+})
+
 
 console.log('Hello')
